a06db6f4a76ef5d9afa18128a302696582b8d4c7
Syntaf <gmercer015@gmail.com>
2015-10-13 21:50:00 -0700

    refactor to include rustty

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,34 @@
+extern crate rustty; 
+
 use ruleset::Ruleset;
-use board::Board;
+
+use self::rustty::{
+    Terminal,
+    Event,
+    HasSize,
+    CellAccessor
+};
 
 pub struct Game<'a> {
-    board: Board,
+    term: Terminal,
     ruleset: &'a Ruleset
+    
 }
 
 impl<'a> Game<'a> {
-    pub fn new(t_board: Board, t_ruleset: &'a Ruleset) -> Game {
-        Game { board: t_board, ruleset: t_ruleset }
+    pub fn new(t_term: Terminal, t_ruleset: &'a Ruleset) -> Game {
+        Game { term: t_term, ruleset: t_ruleset }
+    }
+
+    pub fn run(&mut self) {
+        'main: loop {
+            while let Some(Event::Key(ch)) = self.term.get_event(0).unwrap() {
+                match ch {
+                    'q' => break 'main,
+                     _  => {},
+                }
+            }
+        }
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,21 @@
  * Any cell that is alive and has four of more neighbors gets smothered and dies
  * Any cell that is dead and has exactly three neighbors is born, and alive
 */
+extern crate rustty;
 
-pub mod board;
 pub mod ruleset;
 pub mod game;
 
-use board::Board;
 use ruleset::Ruleset;
 use game::Game;
 
 use std::io::{self, BufRead};
 
 fn main() {
-    // Create reader, prompt user and grab a line
-    let mut reader = io::stdin();
-    let mut input_text = String::new();
-    match reader.read_line(&mut input_text) {
-        Err(e)  => { panic!("Error {}", e); },
-        Ok(_)   => { }
-    }
-
-    // Split line into white space separated sections
-    let mut input_iter = input_text.split_whitespace().map(
-        |x| x.trim().parse::<usize>().ok().expect("Error, invalid integer detected"));
-    let (a, b) = (input_iter.next().unwrap(), input_iter.next().unwrap());
-
-    let mut board = Board::new(a, b);
+    let mut term = rustty::Terminal::new().unwrap();
     let ruleset = Ruleset::new(ruleset::DEFAULT);
-    let mut game = Game::new(board, &ruleset);
-
+    let mut game = Game::new(term, &ruleset);
+    game.run();
 }
 
 
