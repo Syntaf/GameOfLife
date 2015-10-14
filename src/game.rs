8eb19411ecb6027d6e130b28be7f7cf9aa9fedd9
Syntaf <gmercer015@gmail.com>
2015-10-14 09:52:36 -0700

    UI layout created

--- src/game.rs
+++ src/game.rs
@@ -9,15 +9,32 @@ use self::rustty::{
     CellAccessor
 };
 
+use self::rustty::ui::{
+    Painter,
+    Dialog,
+    DialogResult,
+    Alignable,
+    HorizontalAlign,
+    VerticalAlign,
+    Widget
+};
+
 pub struct Game<'a> {
     term: Terminal,
-    ruleset: &'a Ruleset
-    
+    ruleset: &'a Ruleset,
+    ui: Dialog,
+    canvas: Widget
 }
 
 impl<'a> Game<'a> {
     pub fn new(t_term: Terminal, t_ruleset: &'a Ruleset) -> Game {
-        Game { term: t_term, ruleset: t_ruleset }
+        let mut ui_dlg = Dialog::new(t_term.cols(), 10);
+        let mut canvas = Widget::new(t_term.cols(), t_term.rows() - 10);
+        ui_dlg.window_mut().align(&t_term, HorizontalAlign::Left, VerticalAlign::Bottom, 0);
+        ui_dlg.window_mut().draw_box();
+        canvas.align(&t_term, HorizontalAlign::Left, VerticalAlign::Top, 0);
+        canvas.draw_box();
+        Game { term: t_term, ruleset: t_ruleset, ui: ui_dlg, canvas: canvas }
     }
 
     pub fn run(&mut self) {
@@ -28,6 +45,10 @@ impl<'a> Game<'a> {
                      _  => {},
                 }
             }
+
+            self.ui.window().draw_into(&mut self.term);
+            self.canvas.draw_into(&mut self.term);
+            self.term.swap_buffers().unwrap();
         }
     }
 }
