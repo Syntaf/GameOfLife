6ec388b298009ac5724f4133304ea5146cb832e1
Syntaf <gmercer015@gmail.com>
2015-11-16 20:21:42 -0500

    Add buttons for UI

--- src/game.rs
+++ src/game.rs
@@ -33,26 +33,67 @@ impl<'a> Game<'a> {
     pub fn new(t_term: Terminal, t_ruleset: &'a Ruleset) -> Game {
         let (t_width, t_height) = t_term.size();
 
-        let mut dlg = Dialog::new(t_width, t_height/5);
+        if(t_width < 50 || t_height < 30) {
+            let msg = format!("Terminal must be larger than 50 columns and \
+            30 rows. {}x{} detected", t_width, t_height);
+            panic!(msg);
+        }
+
+        let mut ui_ = Game::create_ui(t_width, t_height/5);
+        ui_.pack(&t_term, HorizontalAlign::Middle, VerticalAlign::Bottom, (0,0));
+
+        let mut canvas_ = Canvas::new(t_width, t_height - t_height/5);
+        canvas_.draw_box();
+        canvas_.pack(&t_term, HorizontalAlign::Middle, VerticalAlign::Top, (0,0));
+
+        Game { 
+            term: t_term, 
+            ruleset: t_ruleset, 
+            ui: ui_, 
+            canvas: canvas_ 
+        }
+    }
+
+    fn create_ui(width: usize, height: usize) -> Dialog {
+        let mut dlg = Dialog::new(width, height);
         dlg.draw_box();
 
         let mut title = Label::from_str("Welcome to the console based game of life!");
         title.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (1,1));
         dlg.add_label(title);
-        dlg.pack(&t_term, HorizontalAlign::Middle, VerticalAlign::Bottom, (0,0));
+        
+        let mut play = StdButton::new("Play", 'p', ButtonResult::Custom(2));
+        play.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Bottom, (2,5));
+        dlg.add_button(play);
 
-        let mut canvas_ = Canvas::new(t_width - 1, t_height - t_height/5 - 1);
-        canvas_.draw_box();
-        canvas_.pack(&t_term, HorizontalAlign::Middle, VerticalAlign::Top, (0,1));
+        let mut stop = StdButton::new("Stop", 's', ButtonResult::Custom(3));
+        stop.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Bottom, (2,4));
+        dlg.add_button(stop);
+        
+        let mut settings = StdButton::new("Settings", 's', ButtonResult::Custom(1));
+        settings.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Bottom, (2,3));
+        dlg.add_button(settings);
 
-        Game { term: t_term, ruleset: t_ruleset, ui: dlg, canvas: canvas_ }
+        let mut quit = StdButton::new("Quit", 'q', ButtonResult::Ok);
+        quit.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Bottom, (2,2));
+        dlg.add_button(quit);
+
+        let mut help = StdButton::new("Help", 'h', ButtonResult::Custom(4));
+        help.pack(&dlg, HorizontalAlign::Middle, VerticalAlign::Bottom, (0, 5));
+        dlg.add_button(help);
+
+        let mut about = StdButton::new("About", 'a', ButtonResult::Custom(5));
+        about.pack(&dlg, HorizontalAlign::Middle, VerticalAlign::Bottom, (0, 4));
+        dlg.add_button(about);
+
+        dlg
     }
 
     pub fn run(&mut self) {
         'main: loop {
             while let Some(Event::Key(ch)) = self.term.get_event(0).unwrap() {
-                match ch {
-                    'q' => break 'main,
+                match self.ui.result_for_key(ch) {
+                    Some(ButtonResult::Ok) => break 'main,
                      _  => {},
                 }
             }
