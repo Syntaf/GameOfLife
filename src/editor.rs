860463047160f876aaee6c7d67ce2ef9b8261454
Syntaf <gmercer015@gmail.com>
2015-12-16 00:43:06 -0500

    Write additional options for editor

--- src/editor.rs
+++ src/editor.rs
@@ -5,7 +5,6 @@ use rustty::{
     Event,
     HasSize,
     CellAccessor,
-    Cell,
     Color,
 };
 
@@ -24,9 +23,7 @@ use rustty::ui::{
 
 struct Cursor {
     pos: Position,
-    lpos: Position,
     color: Color,
-    lcolor: Color,
 }
 
 #[derive(Copy, Clone)]
@@ -37,11 +34,13 @@ struct Position {
 
 pub fn open(grid: &mut Grid, term: &mut Terminal) {
     let mut cursor = Cursor {
-        pos: Position { x: 2, y: 2 },
-        lpos: Position { x: 2, y: 2},
+        pos: Position { x: 1, y: 1 },
         color: Color::Default,
-        lcolor: Color::Default,
     };
+    let (t_width, t_height) = term.size();
+
+    let mut ui = create_ui(2 * t_width/3, t_height/5);
+    ui.pack(term, HorizontalAlign::Left, VerticalAlign::Bottom, (0,0));
 
     let (cols, rows) = grid.playable_size();
     term.swap_buffers().unwrap();
@@ -49,7 +48,11 @@ pub fn open(grid: &mut Grid, term: &mut Terminal) {
         while let Some(Event::Key(ch)) = term.get_event(0).unwrap() {
             let canvas = grid.canvas_mut();
             match ch {
-                'q' => { break 'main; },
+                'q' => { 
+                    canvas.get_mut(cursor.pos.x, cursor.pos.y).unwrap().
+                        set_bg(cursor.color);
+                    break 'main; 
+                },
                 'w' => { 
                     canvas.get_mut(cursor.pos.x, cursor.pos.y).unwrap().
                         set_bg(cursor.color);
@@ -78,28 +81,77 @@ pub fn open(grid: &mut Grid, term: &mut Terminal) {
                     cursor.color = canvas.get_mut(cursor.pos.x, cursor.pos.y).
                         unwrap().bg();
                 },
+                '\x7f' => {
+                    cursor.color = Color::Default;
+                    canvas.get_mut(cursor.pos.x, cursor.pos.y).unwrap().set_bg(
+                        cursor.color);
+                }
                 '\r' => {
                     cursor.color = Grid::rand_color();
                     canvas.get_mut(cursor.pos.x, cursor.pos.y).unwrap().set_bg(
                         cursor.color);
                 },
+                'c' => {
+                    for y in 1..rows {
+                        for x in 1..cols {
+                            canvas.get_mut(x, y).unwrap().
+                                set_bg(Color::Default);
+                        }
+                    }
+                }
                 _ => {}
             }
         }
         if cursor.pos.x > cols {
             cursor.pos.x -= 1;
+        } else if cursor.pos.x < 1 {
+            cursor.pos.x += 1;
         }
         if cursor.pos.y > rows {
             cursor.pos.y -= 1;
+        } else if cursor.pos.y < 1 { 
+            cursor.pos.y += 1;
         }
-        //grid.canvas_mut().get_mut(cursor.lpos.x, cursor.lpos.y).unwrap().set_bg(
-        //    cursor.lcolor);
         grid.canvas_mut().get_mut(cursor.pos.x, cursor.pos.y).unwrap().set_bg(
             Color::Red);
 
-        //cells[(cursor.lpos.x, cursor.lpos.y)].set_bg(cursor.lcolor);
-        //cells[(cursor.pos.x, cursor.pos.y)].set_bg(cursor.color);
         grid.draw(term);
+        ui.draw(term);
         term.swap_buffers().unwrap();
     }
+}
+
+fn create_ui(width: usize, height: usize) -> Dialog {
+    let mut dlg = Dialog::new(width, height);
+    dlg.draw_box();
+
+    let mut title = Label::from_str("Editor Mode Controls");
+    title.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (2,1));
+    dlg.add_label(title);
+   
+    let mut movement = Label::from_str(
+        format!("{:<10} {}","w,a,s,d", "-> Move about the board"));
+    movement.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (2,3));
+    dlg.add_label(movement);
+
+    let mut create = Label::from_str(
+        format!("{:<10} {}","Enter", "-> Set cell to random color"));
+    create.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (2,4));
+    dlg.add_label(create);
+
+    let mut del = Label::from_str(
+        format!("{:<10} {}","Backspace", "-> Set cell to default color"));
+    del.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (2,5));
+    dlg.add_label(del);
+
+    let mut clear = Label::from_str(
+        format!("{:<10} {}","c", "-> Clear screen"));
+    clear.pack(&dlg, HorizontalAlign::Left, VerticalAlign::Top, (50,3));
+    dlg.add_label(clear);
+
+    let mut quit = StdButton::new("Quit", 'q', ButtonResult::Ok);
+    quit.pack(&dlg, HorizontalAlign::Right, VerticalAlign::Bottom, (2,1));
+    dlg.add_button(quit);
+
+    dlg
 }
